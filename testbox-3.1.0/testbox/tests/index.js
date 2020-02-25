90e77123eb9cf14fcf2983b427ab7ad4459f2608
sebas <stimotev11@gmail.com>
2020-02-24 23:51:51 -0500

    first commit

---
+++ testbox-3.1.0/testbox/tests/index.js
@@ -0,0 +1,16 @@
+function fizzBuzz(number) {
+    if (number % 15 === 0) {
+        return 'fizzBuzz';
+    }
+    if (number % 3 === 0) {
+        return 'Fizz';
+    }
+    if (number % 5 === 0) {
+        return 'Buzz';
+    }
+
+    return number;
+
+}
+
+module.exports = fizzBuzz;
